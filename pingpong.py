b214e907368cd0125df64753e01af74faf631c35
Nabill <naabill083@gmail.com>
2025-12-12 21:25:49 +0700

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -6,14 +6,81 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #e.g. 55,55 - parameters
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+    
+class Player(GameSprite):
+    def update_L(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+            
+    def update_R(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+            
 clock= time.Clock()
 FPS = 60
 game = True
+paddle1 = Player('paddle.png', 30, 200, 4, 50, 150)
+paddle2 = Player('paddle.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-            
+    if finish != True:
+        window.fill(back)
+        paddle1.update_L()
+        paddle2.update_R()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    
+    if sprite.collide_rect(paddle1, ball) or sprite.collide_rect(paddle2, ball):
+        speed_x *= -1
+        
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+    
+    if ball.rect.x > win_width-50:
+        finish = True
+        window.blit(lose2, (200, 200))
+
+
+        
+    paddle1.reset()
+    paddle2.reset()
+    ball.reset()
+
     display.update()
     clock.tick(FPS)
+    
